71fee8614afe284b51c31731db2a61a5aa5597fd
Dimitri Stiliadis <dstiliadis@gmail.com>
2018-01-13 10:46:10 -0800

    give error

--- enforcer/datapath/datapath_tcp.go
+++ enforcer/datapath/datapath_tcp.go
@@ -325,7 +325,7 @@ func (d *Datapath) processApplicationSynAckPacket(tcpPacket *packet.Packet, cont
 	// We can also clean up the state since we are not going to see any more
 	// packets from this connection.
 	if conn.GetState() == connection.TCPData && !conn.ServiceConnection {
-		cmd := "conntrack -U -p tcp --sport " + strconv.Itoa(int(tcpPacket.SourcePort)) + " -d " + tcpPacket.DestinationAddress.String() + " -s " + tcpPacket.SourceAddress.String()
+		cmd := "conntrack -U -p tcp --sport " + strconv.Itoa(int(tcpPacket.DestinationPort)) + " -d " + tcpPacket.DestinationAddress.String() + " -s " + tcpPacket.SourceAddress.String()
 		if _, err := execConntrack(cmd); err != nil {
 			zap.L().Error("FAILED in conntrack", zap.Error(err), zap.String("command", cmd))
 		}
